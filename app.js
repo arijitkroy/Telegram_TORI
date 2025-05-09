d439737a10ca104efe4ce7af66643cf4f6e79061
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-05-09 09:26:57 +0530

    .

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const { TOKEN, API_URL } = require("./config");
 const handler = require("./utils/handler");
 let offset = 0;
 
+axios.get(`${API_URL}/deleteWebhook`)
+    .then(() => console.log('✅ Webhook deleted'))
+    .catch(err => console.error('Webhook delete error:', err.message));
+
 async function sendMessage(chatId, text) {
     try {
         await axios.post(`${API_URL}/sendMessage`, {
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const axios = require("axios");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -7,14 +6,6 @@ const getUpdates = require("./app");
 app.get('/', (req, res) => {
     res.send("Bot is running!");
 });
-
-// setInterval(() => {
-//   axios.get(`https://telegram-tori.onrender.com/`).then(() => {
-//     console.log("🔄 Self-ping to prevent sleep");
-//   }).catch((err) => {
-//     console.error("⚠️ Self-ping failed:", err.message);
-//   });
-// }, 1000 * 60 * 5);
 
 getUpdates();
 app.listen(PORT);
