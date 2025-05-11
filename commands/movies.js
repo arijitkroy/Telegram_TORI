44fed626cc759366064d448c59abb36c67a8fd6a
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-05-11 22:16:53 +0530

    Update movies.js

--- commands/movies.js
+++ commands/movies.js
@@ -22,7 +22,8 @@ module.exports = async function movies(chatId, userMessage, sendMessage) {
             sendMessage("⚠️ Cannot find the movie!");
             return;
         }
-        data.data.forEach(async (entries) => {
+        console.log(data.data);
+        data.data.data.forEach(async (entries) => {
             let imageUrl = entries.poster;
             let caption = `Name: ${entries.name}\nGenre: ${entries.genre}\nDate: ${entries.date}\nRating: ${entries.rating}\nRuntime: ${entries.runtime}\nDownloads:\n1. [${entries.torrents[0].quality}](${entries.torrents[0].torrent}) - ${entries.torrent[0].size}\n2. [${entries.torrents[1].quality}](${entries.torrents[1].torrent}) - ${entries.torrent[1].size}\n\nView more details [here](${entries.url})`;
 
