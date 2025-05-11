0bea3a7dbfaa808ba55f6749ca2c5258de35b6e5
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-05-11 22:29:42 +0530

    Update movies.js

--- commands/movies.js
+++ commands/movies.js
@@ -30,7 +30,7 @@ module.exports = async function movies(chatId, userMessage, sendMessage) {
         console.log(data.data);
         data.forEach(async (entries) => {
             let imageUrl = entries.poster;
-            let caption = `Name: ${escapeMarkdown(entries.name)}\nGenre: ${escapeMarkdown(entries.genre)}\nDate: ${escapeMarkdown(entries.date)}\nRating: ${escapeMarkdown(entries.rating)}\nRuntime: ${escapeMarkdown(entries.runtime)}\nDownloads:\n1. [${entries.torrents[0].quality}](${entries.torrents[0].torrent}) - ${escapeMarkdown(entries.torrents[0].size)}\n2. [${entries.torrents[1].quality}](${entries.torrents[1].torrent}) - ${escapeMarkdown(entries.torrents[1].size)}\n\nView more details [here](${entries.url})`;
+            let caption = `Name: ${escapeMarkdown(entries.name)}\nGenre: ${entries.genre}\nDate: ${escapeMarkdown(entries.date)}\nRating: ${escapeMarkdown(entries.rating)}\nRuntime: ${escapeMarkdown(entries.runtime)}\nDownloads:\n1. [${entries.torrents[0].quality}](${entries.torrents[0].torrent}) - ${escapeMarkdown(entries.torrents[0].size)}\n2. [${entries.torrents[1].quality}](${entries.torrents[1].torrent}) - ${escapeMarkdown(entries.torrents[1].size)}\n\nView more details [here](${entries.url})`;
 
             const imageResponse = await axios.get(imageUrl, { responseType: 'stream' });
             const fileName = path.basename(imageUrl.split('?')[0]);
