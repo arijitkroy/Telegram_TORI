e22a9e5f346e4c9518cf726ba7c81a7372a2fe79
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2025-05-11 22:27:01 +0530

    Update movies.js

--- commands/movies.js
+++ commands/movies.js
@@ -3,6 +3,11 @@ const FormData = require('form-data');
 const path = require("path");
 const { API_URL } = require("../config");
 
+function escapeMarkdown(text) {
+    return text
+        .replace(/[_*[\]()~`>#+\-=|{}.!]/g, '\\$&');
+}
+
 module.exports = async function movies(chatId, userMessage, sendMessage) {
     const args = userMessage.replace('/movies', '').trim();
     if (!args) {
@@ -10,22 +15,22 @@ module.exports = async function movies(chatId, userMessage, sendMessage) {
         return;
     }
     const url = `https://torrent-api-py-nx0x.onrender.com/api/v1/search?site=yts&query=${encodeURIComponent(args)}&limit=4`;
-    await sendMessage('🔎 Looking up your movie!');
+    sendMessage("🔎 Looking up your movie!");
     try {
-        const data = await axios.get(url, {
+        const data = (await axios.get(url, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Telegram Bot)',
                 'Content-Type': 'application/json'
             }
-        });
+        })).data.data;
         if (!data) {
             sendMessage("⚠️ Cannot find the movie!");
             return;
         }
         console.log(data.data);
-        data.data.data.forEach(async (entries) => {
+        data.forEach(async (entries) => {
             let imageUrl = entries.poster;
-            let caption = `Name: ${entries.name}\nGenre: ${entries.genre}\nDate: ${entries.date}\nRating: ${entries.rating}\nRuntime: ${entries.runtime}\nDownloads:\n1. [${entries.torrents[0].quality}](${entries.torrents[0].torrent}) - ${entries.torrents[0].size}\n2. [${entries.torrents[1].quality}](${entries.torrents[1].torrent}) - ${entries.torrents[1].size}\n\nView more details [here](${entries.url})`;
+            let caption = `Name: ${escapeMarkdown(entries.name)}\nGenre: ${escapeMarkdown(entries.genre)}\nDate: ${escapeMarkdown(entries.date)}\nRating: ${escapeMarkdown(entries.rating)}\nRuntime: ${escapeMarkdown(entries.runtime)}\nDownloads:\n1. [${entries.torrents[0].quality}](${entries.torrents[0].torrent}) - ${escapeMarkdown(entries.torrents[0].size)}\n2. [${entries.torrents[1].quality}](${entries.torrents[1].torrent}) - ${escapeMarkdown(entries.torrents[1].size)}\n\nView more details [here](${entries.url})`;
 
             const imageResponse = await axios.get(imageUrl, { responseType: 'stream' });
             const fileName = path.basename(imageUrl.split('?')[0]);
@@ -33,6 +38,7 @@ module.exports = async function movies(chatId, userMessage, sendMessage) {
             const form = new FormData();
             form.append('chat_id', chatId);
             form.append('caption', caption);
+            form.append('parse_mode', 'MarkdownV2');
             form.append('photo', imageResponse.data, {
                 filename: fileName
             });
